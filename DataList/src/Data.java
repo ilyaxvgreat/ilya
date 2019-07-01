35b25dddf099ffaa1d175be634578f354b288921
ilyaxvgreat <50105231+ilyaxvgreat@users.noreply.github.com>
2019-07-01 21:42:16 +0300

    have made clear code

--- DataList/src/Data.java
+++ DataList/src/Data.java
@@ -1,21 +1,11 @@
 import java.util.*;
 
 public class Data {
-    //      a1 = {1, 0};
-//      a2 = {2, 7};
-//      a3 = {-1, 3};
-//      a4 = {7, 2};
-//      a5 = {3, -1};
-//      a6 = {7, 2};
-//      a7 = {0, 8};
-//      a8 = {7, 2};
-//      a9 = {3, -1};
-//     a10 = {-1, 3};
 
     private Map<Integer, Integer> data = new HashMap<Integer, Integer>();
 
-
     public void putInHashMap() {
+
         data.put(1, 0);
         data.put(2, 7);
         data.put(-1, 3);
@@ -31,14 +21,9 @@ public class Data {
     public void test() {
         for (Map.Entry<Integer, Integer> entry : data.entrySet()) {
             if (data.containsKey(entry.getValue()) && data.containsValue(entry.getKey())) {
-                System.out.println("Good");
-                System.out.println( data.containsKey(entry.getValue()) + " " + data.containsValue(entry.getKey()));
+                System.out.println("[ " + entry.getKey() + " ; " + entry.getValue() + " ]" +
+                        "[ " + entry.getValue() + " ; " + entry.getKey() + " ]");
             }
         }
-    }
-
-    private List<Integer> reverse(List<Integer> list) {
-        Collections.reverse(list);
-        return list;
     }
 }
