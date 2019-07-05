4eb79722cce456017a246986c9a7db1b0787587f
ilyaxvgreat <50105231+ilyaxvgreat@users.noreply.github.com>
2019-07-05 18:31:10 +0300

    last

--- DataList/src/Data.java
+++ DataList/src/Data.java
@@ -1,29 +1,91 @@
+import javax.swing.text.html.HTMLDocument;
+import java.awt.List;
+import java.lang.reflect.Array;
+import java.security.KeyStore;
 import java.util.*;
 
-public class Data {
 
-    private Map<Integer, Integer> data = new HashMap<Integer, Integer>();
+public class Data {
+//    public Integer a1 = new int[1, 0];
+//      a2 = {2, 7};
+//      a3 = {-1, 3};
+//      a4 = {7, 2};
+//      a5 = {3, -1};
+//      a6 = {7, 2};
+//      a7 = {0, 8};
+//      a8 = {7, 2};
+//      a9 = {3, -1};
+//     a10 = {-1, 3};
+
+    private HashMap<NumbersPair, Integer> data = new HashMap<>();
 
     public void putInHashMap() {
-
-        data.put(1, 0);
-        data.put(2, 7);
-        data.put(-1, 3);
-        data.put(7, 2);
-        data.put(3, -1);
-        data.put(7, 2);
-        data.put(0, 8);
-        data.put(7, 2);
-        data.put(3, -1);
-        data.put(-1, 3);
+        data.put(new NumbersPair(1, 0), 1);
+        data.put(new NumbersPair(2, 7), 1);
+        data.put(new NumbersPair(-1, 3), 1);
+        data.put(new NumbersPair(7, 2), 1);
+        data.put(new NumbersPair(3, -1), 1);
+        data.put(new NumbersPair(7, 2), 1);
+        data.put(new NumbersPair(0, 8), 1);
+        data.put(new NumbersPair(7, 2), 1);
+        data.put(new NumbersPair(3, -1), 1);
+        data.put(new NumbersPair(-1, 3), 1);
     }
 
     public void test() {
-        for (Map.Entry<Integer, Integer> entry : data.entrySet()) {
-            if (data.containsKey(entry.getValue()) && data.containsValue(entry.getKey())) {
-                System.out.println("[ " + entry.getKey() + " ; " + entry.getValue() + " ]" +
-                        "[ " + entry.getValue() + " ; " + entry.getKey() + " ]");
+        
+        for (Map.Entry<NumbersPair, Integer> i : data.entrySet()) {
+            for (Map.Entry<NumbersPair, Integer> j : data.entrySet()) {
+                if (i.getKey().getSecondNumber() == j.getKey().getFirstNumber() && i.getKey().getFirstNumber() == j.getKey().getSecondNumber()) {
+                    int value;
+                    value = data.get(i);
+                    value++;
+                    data.put((NumbersPair) i, value);
+                    data.remove(j);
+                    System.out.println("GOOD ");
+
+                }
             }
         }
+
+//        for (NumbersPair i : data.keySet()) {
+//            for (NumbersPair j : data.keySet()) {
+//                if (i.getFirstNumber() == j.getFirstNumber() && i.getSecondNumber() == j.getSecondNumber()) {
+//                    int value = data.get(i);
+//                    value++;
+//                    data.put(i, value);
+//                    data.remove(j);
+//                    System.out.println("GOOD " + i.getFirstNumber() + " " + i.getSecondNumber() + " " + j.getSecondNumber() + " " + j.getFirstNumber() + data.);
+//
+//                    break;
+//                }
+//            }
+//        }
+//                System.out.println("[ " + entry.getKey() + " ; " + entry.getValue() + " ]" +
+//                        "[ " + entry.getValue() + " ; " + entry.getKey() + " ]");
     }
 }
+
+class NumbersPair {
+
+    private int firstNumber;
+    private int secondNumber;
+
+    public NumbersPair(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public NumbersPair() {
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+}
+
